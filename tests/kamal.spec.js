5243b8b0dd5b54a299fb85d85405be34938358f0
Kamal <kamalgahtori22@gmail.com>
2025-08-26 22:47:52 +0530

    Modified test and added page object

--- tests/kamal.spec.js
+++ tests/kamal.spec.js
@@ -3,5 +3,6 @@ import { test, expect } from '@playwright/test'
 test.describe("My Tests", () => {
     test("My first test", async ({ page }) => {
         await page.goto("https://www.google.com/")
+
     })
 })
