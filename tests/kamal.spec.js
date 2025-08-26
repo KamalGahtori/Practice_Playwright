282f352576217f27e2ffd5e9facafa44810366e2
Kamal <kamalgahtori22@gmail.com>
2025-08-26 10:35:43 +0530

    added describe block

--- tests/kamal.spec.js
+++ tests/kamal.spec.js
@@ -1,9 +1,12 @@
 import { test, expect } from "@playwright/test";
 import { Kamal } from "../page/kamal";
 let kamal;
+test.describe("My first block", () => {
 
-test("My first test", async ({ page }) => {
-    kamal = new Kamal(page);
-    await kamal.openURL("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-    await kamal.validateURL("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+    test("My first test", async ({ page }) => {
+        kamal = new Kamal(page);
+        await kamal.openURL("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        await kamal.validateURL("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    })
 })
