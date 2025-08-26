4aafdfe66e087485213757a2213d7bf7c12fc5e8
Kamal <kamalgahtori22@gmail.com>
2025-08-26 22:25:10 +0530

    Added new test

---
+++ tests/kamal.spec.js
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test'
+
+test.describe("My Tests", () => {
+    test("My first test", async ({ page }) => {
+        await page.goto("https://www.google.com/")
+    })
+})
